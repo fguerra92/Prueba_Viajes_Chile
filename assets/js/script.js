de4d68684972f092a2f403ff79e3c411959732c7
Felipe <fguerra.ag@gmail.com>
2023-05-07 11:51:10 -0400

    correcciones 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,12 @@
 // CODIGO CON SINTAXIS DE JQUERY
 $(document).ready(function () {
 
-// FUNCION PARA APLICAR TOOLTIP
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-return new bootstrap.Tooltip(tooltipTriggerEl)
-});
-
-
 // Haciendo Clic sobre la imagen de la card desaparece texto (TITULO Y PARRAFO)
 $(".desaparecer").click(function () {
 $(".aparece-desaparece").toggle();
 });
 
 })
-
 
 // Alerta en boton enviar
 let botonEnviar = document.querySelector(".botonEnviar");
